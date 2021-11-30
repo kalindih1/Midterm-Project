caccb3fd96427946e2186181f61df695c4ec1398
Coralgoods <91501799+Coralgoods@users.noreply.github.com>
2021-11-29 19:25:57 -0500

    V1
    V1

---
+++ Midterm_team_exotic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Midterm_team_exotic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
