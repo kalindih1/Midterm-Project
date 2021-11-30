8565e6b1de55e97e20db60b3fa6767cf952208b0
Coralgoods <91501799+Coralgoods@users.noreply.github.com>
2021-11-29 20:26:10 -0500

    Vstart
    Vstart

--- Midterm_team_exotic/Program.cs
+++ Midterm_team_exotic/Program.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 
+
 namespace Midterm_team_exotic
 {
     class Program
     {
         static void Main(string[] args)
-
         {
-            //Eddie
+
             Console.WriteLine("Hello World!");
         }
     }
