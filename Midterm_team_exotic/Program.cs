b6260399744ba368b4c97c7c6c631c9cd6427c03
kalindih1 <73810475+kalindih1@users.noreply.github.com>
2021-12-09 14:12:45 -0500

    new changes

--- Midterm_team_exotic/Program.cs
+++ Midterm_team_exotic/Program.cs
@@ -12,7 +12,7 @@ namespace Midterm_team_exotic
         {
 
 
-            
+
             var myProduct1 = new LineItemData { ProductCategory = "dinner", ProductDescription = "more test data", ProductPrice = 13.50, ProductName = "Cheeseburger", LineItemTotal = 20.50, ProductId = 1, LineItemQuantity = 3, LineItemTax = 40 };
             var myProduct2 = new LineItemData { ProductCategory = "dinner", ProductDescription = "more test data", ProductPrice = 18.50, ProductName = "cookies", LineItemTotal = 30.50, ProductId = 2, LineItemQuantity = 6, LineItemTax = 60 };
 
@@ -20,8 +20,9 @@ namespace Midterm_team_exotic
             var myProduct4 = new LineItemData { ProductCategory = "lunch", ProductDescription = "more test data", ProductPrice = 15.50, ProductName = "coke", LineItemTotal = 30.50, ProductId = 2, LineItemQuantity = 6, LineItemTax = 60 };
             var lunchProducts = new List<LineItemData> { myProduct3, myProduct4, };
             var allProducts = new List<LineItemData> { myProduct1, myProduct2, myProduct3, myProduct4 };
-           
-            PaymentMethod(lunchProducts);
+
+            PaymentMethod(allProducts);
+            
 
 
 
@@ -31,7 +32,7 @@ namespace Midterm_team_exotic
         {
 
 
-            Console.WriteLine("Would you like to pay with Cash, Check or Credit?");
+            Console.WriteLine("Would you like to pay with Cash, Check or Credit?\n");
             string checkUserInput = Console.ReadLine().Trim().ToLower();
 
             if (checkUserInput == "cash")
@@ -49,8 +50,8 @@ namespace Midterm_team_exotic
             }
             else
             {
-                Console.WriteLine("Please check your spelling or select payment method again");
-                Console.WriteLine("Would you like to pay with Cash, Check or Credit?");
+                Console.WriteLine("Please check your spelling or select payment method again\n");
+                Console.WriteLine("Would you like to pay with Cash, Check or Credit?\n");
                 checkUserInput = Console.ReadLine().Trim().ToLower();
 
             }
@@ -58,83 +59,86 @@ namespace Midterm_team_exotic
 
         public static void AcceptCash(List<LineItemData> products)
         {
-            Console.WriteLine("You've chosen cash");
+            Console.WriteLine("You've chosen cash\n");
             //calculate total of the products
             double totalProductCost = CalculatePaymentTotal(products);
-            Console.WriteLine("Your total cost is : " + totalProductCost);
-            Console.WriteLine("Please enter tendered amount");
+            Console.WriteLine("Your total cost is : " + totalProductCost + "\n");
+            Console.WriteLine("Please enter tendered amount\n");
 
             double cashAmount;
-            while (double.TryParse(Console.ReadLine(),out cashAmount) == false)
+            while (double.TryParse(Console.ReadLine(), out cashAmount) == false)
             {
-                Console.WriteLine("Not a valid input. Please input your amount.");
+                Console.WriteLine("Not a valid input. Please input your amount.\n");
             }
             if (cashAmount > totalProductCost)
             {
-                Console.WriteLine("This is your amount given : " + cashAmount);
+                Console.WriteLine("This is your amount given : " + cashAmount + "\n");
             }
             else
             {
-                Console.WriteLine("You don't have enough money.");
+                Console.WriteLine("You don't have enough money.\n");
                 return;
             }
             //subtract total from cashAmount
             double change = cashAmount - totalProductCost;
-            Console.WriteLine("Your change is : " + change);
-            PrintCashReceipt(products);
+            Console.WriteLine("Your change is : " + change + "\n");
+            PrintReceipt(products, "cash");
+
         }
         public static void AcceptCheck(List<LineItemData> products)
         {
-            Console.WriteLine("You've chosen check");
-            Console.WriteLine("Please enter your check number");
+            Console.WriteLine("You've chosen check\n");
+            Console.WriteLine("Please enter your check number\n");
 
             // TODO: make sure to ask instructor what is a valid check number
             int checkNumber;
             while (int.TryParse(Console.ReadLine(), out checkNumber) == false)
             {
-                Console.WriteLine("Not a valid input. Please input your check number");
+                Console.WriteLine("Not a valid input. Please input your check number\n");
             }
-            Console.WriteLine("This is your check number " + checkNumber);
+            Console.WriteLine("This is your check number " + checkNumber + "\n");
+            PrintReceipt(products, "check");
         }
         public static void AcceptCredit(List<LineItemData> products)
-        {   
-            Console.WriteLine("You've chosen credit");
+        {
+            Console.WriteLine("You've chosen credit\n");
             //Take in credit card number
-            Console.WriteLine("Please enter your credit card number");
+            Console.WriteLine("Please enter your credit card number\n");
 
             string cardNumber = Console.ReadLine().Trim();
             cardNumber = cardNumber.Replace(" ", "");
-            
+
             while (cardNumber.All(char.IsDigit) == false || cardNumber.Length != 16)
             {
-                
-                Console.WriteLine("Not a valid input. Make sure you entered 16 numbers");
+
+                Console.WriteLine("Not a valid input. Make sure you entered 16 numbers\n");
                 cardNumber = Console.ReadLine().Trim();
                 cardNumber = cardNumber.Replace(" ", "");
             }
-            
+
             //Take in expiration date
-            Console.WriteLine("Please enter your expiration date in the form 'MM/YY' ");
+            Console.WriteLine("Please enter your expiration date in the form 'MM/YY' \n");
             string expirationDate = Console.ReadLine().Trim();
 
             Regex expDatePattern = new Regex(@"^(0[1-9]|1[0-2])\/?([0-9]{4}|[0-9]{2})$");
             Match match = expDatePattern.Match(expirationDate);
-            while (match.Success == false )
+            while (match.Success == false)
             {
-                Console.WriteLine("Not a valid input.Make sure you entered the date in the correct format");
+                Console.WriteLine("Not a valid input.Make sure you entered the date in the correct format\n");
                 expirationDate = Console.ReadLine().Trim();
                 match = expDatePattern.Match(expirationDate);
             }
-            
+
             //Take in cw number
-            Console.WriteLine("Please enter your cw number");
+            Console.WriteLine("Please enter your cw number\n");
             string cardCw = Console.ReadLine().Trim();
             while (cardCw.All(char.IsDigit) == false || cardCw.Length != 3)
             {
-                Console.WriteLine("Not a valid input. Make sure you entered 3 numbers");
+                Console.WriteLine("Not a valid input. Make sure you entered 3 numbers\n");
                 cardCw = Console.ReadLine().Trim();
             }
-            
+            PrintReceipt(products, "credit");
+
         }
 
         public static double CalculatePaymentTotal(List<LineItemData> products)
@@ -147,6 +151,18 @@ namespace Midterm_team_exotic
             return totalProductCost;
         }
 
+        public static double CalculatePaymentWithTax(List<LineItemData> products)
+        {
+            const double tax = 0.6;
+            double totalProductCost = 0;
+            foreach (LineItemData product in products)
+            {
+                totalProductCost = totalProductCost + (product.ProductPrice * tax);
+            }
+            return totalProductCost;
+
+        }
+
        public static void PrintSubTotal(List<LineItemData> products)
         {
             Dictionary<string, List<LineItemData>> categoryDictionary = new Dictionary<string, List<LineItemData>>();
@@ -154,53 +170,43 @@ namespace Midterm_team_exotic
             foreach (KeyValuePair<string, List<LineItemData>> entry in categoryDictionary)
             {
                 Console.Write("Category : ");
-                Console.WriteLine(entry.Key);
+                Console.WriteLine(entry.Key + "\n");
+                Console.Write("This is the subtotal : ");
+                Console.WriteLine(CalculatePaymentTotal(entry.Value)+ "\n");
+
+              
+
             }
+            
 
 
         }
-
-        public static void PrintCashReceipt(List<LineItemData> products)
+        public static void PrintReceipt(List<LineItemData> products, string paymentType)
         {
-           Console.WriteLine("Customer receipt");
 
-           foreach(LineItemData product in products)
+            Console.WriteLine("Customer receipt\n");
+
+            foreach (LineItemData product in products)
             {
                 Console.Write(product.ProductName);
                 Console.Write(" - " + "$" + product.ProductPrice);
                 Console.Write(" - " + product.ProductCategory);
-                Console.WriteLine(" - " + "quantity: " + product.LineItemQuantity);
-                
-               
-                
+                Console.WriteLine(" - " + "quantity: " + product.LineItemQuantity + "\n");
+
+
+
             }
-            Console.WriteLine("This is your sub total : ");
             PrintSubTotal(products);
-            Console.WriteLine("This is your grand total : " + CalculatePaymentTotal(products));
-            Console.WriteLine("You paid with cash");
+            Console.WriteLine("This is your grand total : " + CalculatePaymentWithTax(products)+ "\n");
+            Console.WriteLine("You paid with " + paymentType + "\n");
         }
 
-        public static void PrintCreditReceipt(List<LineItemData> products)
-        {
-            foreach (LineItemData product in products)
-            {
-                Console.WriteLine(product.ProductName, product.LineItemTotal, product.ProductCategory, product.LineItemQuantity);
 
-            }
-            Console.WriteLine("This is your total : " + CalculatePaymentTotal(products));
-            Console.WriteLine("You paid with credit");
-        }
+       
 
-        public static void PrintCheckReceipt(List<LineItemData> products)
-        {
-            foreach (LineItemData product in products)
-            {
-                Console.WriteLine(product.ProductName, product.LineItemTotal, product.ProductCategory, product.LineItemQuantity);
+        
 
-            }
-            Console.WriteLine("This is your total : " + CalculatePaymentTotal(products));
-            Console.WriteLine("You paid with check");
-        }
+        
 
         public static Dictionary<string, List<LineItemData>> SeperateCategories(List<LineItemData> products)
         {
@@ -220,22 +226,8 @@ namespace Midterm_team_exotic
                 {
                     categoryDictionary[product.ProductCategory].Add(product); 
                 }
-                
-                
-
             }
-
-
-
             return categoryDictionary;
         }
-
-
-
-
-
-
-
-
     }
 }
